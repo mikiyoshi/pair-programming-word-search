17f49a44be7677229258c68693ff1e511b38b0ad
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-12-01 23:01:15 +0000

    this file updated pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  let newArray = [];
 
-module.exports = wordSearch
+  for (let i = 0; i < letters[0].length; i++) {
+    newArray.push([]);
+    //    console.log(newArray);
+  }
+  for (let row = 0; row < letters.length; row++) {
+    // console.log(letters[k]);
+    for (let col = 0; col < letters[row].length; col++) {
+      let newEl = letters[row][col];
+      newArray[col].push(newEl);
+    }
+  }
+
+  // console.log(newArray);
+  for (let i = 0; i < newArray.length; i++) {
+    // console.log(newArray[i]);
+    const newArr = newArray[i].join('');
+    // console.log(newArr);
+    if (newArr === word) {
+      return true;
+    }
+  }
+
+  ////////////////////////////////////////
+  // console.log(letters);
+  // console.log(word);
+  const horizontalJoin = letters.map((ls) => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
